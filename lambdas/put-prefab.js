f5ff2c80dae0f83c6104a46fe73f0e294fd27e5b
Artur Varosyan <artur.varosyan1@gmail.com>
2022-09-25 21:30:52 +0800

    Add code for uploading files

---
+++ lambdas/put-prefab.js
@@ -0,0 +1,50 @@
+var AWS = require("aws-sdk");
+
+var s3 = new AWS.S3({
+    signatureVersion: 'v4',
+});
+
+exports.handler = (event, context, callback) => {
+
+    const userID = event.queryStringParameters.userID;
+
+    var response
+    if (userID == null) {
+        response = missingUserID();
+    } else {
+        // Check if the user exists in the database
+        // Check whether the use already has a prefab
+        response = generatePresignedURL(userID)
+    }
+    
+    callback(null, response)
+};
+
+function generatePresignedURL(userID) {
+    
+    const url = s3.getSignedUrl('putObject', {
+        Bucket: 'metanoia-prefabs',
+        Key: userID + ".txt",
+        Expires: 600,  // longer expiration for upload
+    })
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            url: url,
+        })
+    }
+
+    return response;
+}
+
+function missingUserID() {
+    const response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            error: "The userID is missing in the request body.",
+        })
+    }
+
+    return response;
+}
