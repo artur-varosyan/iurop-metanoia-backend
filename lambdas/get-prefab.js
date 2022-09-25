7691544863343ede943ff9383fab6a028f07288d
Artur Varosyan <artur.varosyan1@gmail.com>
2022-09-25 19:30:09 +0800

    Add check for parameters

--- lambdas/get-prefab.js
+++ lambdas/get-prefab.js
@@ -1,14 +1,50 @@
-var AWS = require("aws-sdk")
+var AWS = require("aws-sdk");
+
 var s3 = new AWS.S3({
     signatureVersion: 'v4',
 });
 
 exports.handler = (event, context, callback) => {
+
+    const userID = event.userID;
+
+    var response
+    if (userID == null) {
+        response = missingUserID();
+    } else {
+        // Check if the user and their prefab exist in the database
+        response = generatePresignedURL(userID)
+    }
+    
+    callback(null, response)
+};
+
+// TODO: Change file extension to .prefab
+function generatePresignedURL(userID) {
+    
     const url = s3.getSignedUrl('getObject', {
         Bucket: 'metanoia-prefabs',
-        Key: 'test.txt',
+        Key: userID + ".txt",
         Expires: 60,
     });
 
-    callback(null, url);
-};
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            url: url,
+        })
+    }
+
+    return response;
+}
+
+function missingUserID() {
+    const response = {
+        statusCode: 400,
+        body: JSON.stringify({
+            error: "The userID is missing in the request body.",
+        })
+    }
+
+    return response;
+}
